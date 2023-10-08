12cc0f5f1fc08e574a3200f838765ee458e0a535
Michael Stein <info@michaelstein-itb.de>
2023-10-08 21:50:51 +0200

    add autoshift

--- rules.mk
+++ rules.mk
@@ -8,4 +8,4 @@ MXSTEINI_OLED_ENABLE = yes
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-#WS2812_DRIVER = vendor # Use the RP2040's PIO interface
+AUTO_SHIFT_ENABLE = yes
